79a572d9f15e6090ecc023c4ee1eac13ca4419e2
Priya Mathew <pri1229@gmail.com>
2014-04-13 14:32:42 -0500

    Add CORS helper and controller methods

--- app/controllers/exercise_controller.rb
+++ app/controllers/exercise_controller.rb
@@ -1,6 +1,9 @@
 class ExerciseController < ApplicationController
+	skip_before_filter :verify_authenticity_token
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
-	def index
+	def index		
 		@exercises = Exercise.all
 
 		render json: @exercises
