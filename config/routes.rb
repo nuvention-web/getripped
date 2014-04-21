66e66e37ba6a4bae5d3b2a60697e587d4e177070
Priya Mathew <pri1229@gmail.com>
2014-04-21 09:59:49 -0500

    Add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Getripped::Application.routes.draw do
 
   resources :exercise, only: [:index, :show]
 
+  resources :session, only: [:new, :create, :destroy]
+
+  get 'session/signout' => 'session#signout', as: :signout
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
