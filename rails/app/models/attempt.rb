77821eba38d43a9ff9af7584ec00348497aefb45
Priya Mathew <pri1229@gmail.com>
2014-05-08 10:13:05 -0500

    Add algorithm logic to calculate weight

--- rails/app/models/attempt.rb
+++ rails/app/models/attempt.rb
@@ -4,14 +4,45 @@ class Attempt < ActiveRecord::Base
 	has_many :workoutsets
 	before_create :total_reps
 
-	attr_accessor :user, :exercise, :weight, :reps1, :reps2, :reps3
+	# attr_accessible :user, :exercise, :weight, :reps1, :reps2, :reps3
 
 	def total_reps
-		total_reps = reps1 + reps2 + reps3 	
+		total_reps = self.reps1 + self.reps2 + self.reps3 	
 	end
 
-	def calculate_weight(total_reps_P1, total_reps_P2, total_reps_P3)
-		weighted average = total_reps_P1/ + total_reps_P2 + total_reps_P3
+	def self.avg_pct_reps_completed
+		last_three_attempts = Attempt.last(3)
+		total_reps_attempt_1 = last_three_attempts[0].total_reps
+		total_reps_attempt_2 = last_three_attempts[1].total_reps
+		total_reps_attempt_3 = last_three_attempts[2].total_reps
+
+		total_reps_P1 = total_reps_attempt_1.to_f / 36  
+		total_reps_P2 = total_reps_attempt_2.to_f / 36
+		total_reps_P3 = total_reps_attempt_3.to_f / 36
+
+		total_reps_average = (total_reps_P1 + total_reps_P2 + total_reps_P3)/3
+	end
+
+	def last_three_equal?
+		last_three_attempts = Attempt.last(3)
+		weight_used_attempt_1 = last_three_attempts[0].weight
+		weight_used_attempt_2 = last_three_attempts[1].weight
+		weight_used_attempt_3 = last_three_attempts[2].weight
+
+		return true if weight_used_attempt_1 == weight_used_attempt_2 && weight_used_attempt_1== weight_used_attempt_3
+
+	end
+
+	def calculate_weight
+		if last_three_equal?
+			if Attempt.avg_pct_reps_completed > 0.8
+				self.next_weight = Attempt.last.weight + 5
+			elsif Attempt.avg_pct_reps_completed < 0.5
+				self.next_weight = Attempt.last.weight - 5 
+			end
+		end
 	end
 
 end
+
+#add column to exercise for rec_reps in db
