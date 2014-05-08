51750c6b8808c2366f9c1acf4ce2f2d268640fcb
Priya Mathew <pri1229@gmail.com>
2014-05-07 23:35:30 -0500

    Add db changes to support Eric's goal model

--- rails/app/models/attempt.rb
+++ rails/app/models/attempt.rb
@@ -2,4 +2,16 @@ class Attempt < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :exercise
 	has_many :workoutsets
+	before_create :total_reps
+
+	attr_accessor :user, :exercise, :weight, :reps1, :reps2, :reps3
+
+	def total_reps
+		total_reps = reps1 + rep2 + rep3 	
+	end
+
+	def calculate_weight(total_reps_P1, total_reps_P2, total_reps_P3)
+		weighted average = total_reps_P1/ + total_reps_P2 + total_reps_P3
+	end
+
 end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140426005845) do
+ActiveRecord::Schema.define(version: 20140508041144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140426005845) do
     t.integer  "reps3"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "next_weight"
   end
 
   create_table "exercises", force: true do |t|
